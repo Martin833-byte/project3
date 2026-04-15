f20e722964333eb727441708571ee4f1a7ebe3ba
Martin833-byte <mguzman16@hawk.illinoistech.edu>
2026-04-15 18:31:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 document.addEventListener("DOMCOntentLoaded",()=>{
   console.log("JavaScript loaded successfully!");
+  
+  const heading = document.querySelectorAll("h1");
+  
+  headings.forEach(h=> {
+	  h.addEventListener("click,()=> {
+		  h.style.color= h.style.color === "blue"? "black" : "blue";
+	  });
+  });
+});
+
