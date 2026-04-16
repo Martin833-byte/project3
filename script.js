8b1f861b08eaf2668eb76fd81c6c3cb5b632f974
Martin833-byte <mguzman16@hawk.illinoistech.edu>
2026-04-15 19:20:19 -0500

    fixed error

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ document.addEventListener("DOMContentLoaded",()=>{
   
   const heading = document.querySelectorAll("h1");
   
-  headings.forEach(h=> {
-	  h.addEventListener("click,()=> {
+  heading.forEach(h=> {
+	  h.addEventListener("click",()=> {
 		  h.style.color= h.style.color === "blue"? "black" : "blue";
 	  });
   });
